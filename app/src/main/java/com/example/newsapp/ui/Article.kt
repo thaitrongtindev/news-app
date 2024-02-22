bbf2e507240a4a7105aba00881e1a6b529c0b574
thaitrongtin <thaitrongtindev@gmail.com>
2024-02-22 15:04:38 +0700

    setup Entity for class Article

--- app/src/main/java/com/example/newsapp/ui/Article.kt
+++ app/src/main/java/com/example/newsapp/ui/Article.kt
@@ -1,6 +1,13 @@
 package com.example.newsapp.ui
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "articles")
+
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
     val source: Source,
     val author: String,
     val title: String,
