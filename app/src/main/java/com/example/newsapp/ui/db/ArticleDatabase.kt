72d2fe8c529c49525da00a64b0a1babc24bb54e0
thaitrongtin <thaitrongtindev@gmail.com>
2024-02-22 15:55:58 +0700

    create article database

---
+++ app/src/main/java/com/example/newsapp/ui/db/ArticleDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.newsapp.ui.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.newsapp.ui.Article
+
+@Database(entities = [Article::class], version = 1)
+abstract class ArticleDatabase : RoomDatabase() {
+
+    companion object {
+        @Volatile
+        private var instance: ArticleDatabase? = null
+        private val LOCK = Any()
+
+        operator  fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDatabase(context).also { instance = it }
+        }
+
+        private fun createDatabase(context: Context) : ArticleDatabase {
+            return Room.databaseBuilder(context.applicationContext
+            , ArticleDatabase::class.java,
+                "article_db.db")
+                .build()
+        }
+    }
+
+}
