d24cdf89bfcba093badd31c6433a4ef052e6c60a
thaitrongtin <thaitrongtindev@gmail.com>
2024-02-22 09:14:49 +0700

    setup nav

--- app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
@@ -1,11 +1,12 @@
-package com.example.newsapp
+package com.example.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newsapp.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
